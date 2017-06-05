6eaf0062e3244f696ba2a4e650de9ef1f7b7baab
dovesongsong <906110622@qq.com>
2017-06-05 17:18:14 +0800

    打包文件，新建package为data_structure_algorithm，创建setup.py 执行命令：python setup.py sdist 命令执行成功后 执行命令：python setup install 就可以import data_structure_algorithm

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 17:03:07 2017
+
+@author: 248948
+"""
+from distutils.core import setup
+
+setup(name='data_structure_algorithm',
+      version='1.0',
+      description='data structure common algorithm package',
+      author='chen',
+      author_email='15260817193@139.com',
+      packages=['data_structure_algorithm'],
+     )
